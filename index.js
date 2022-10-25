460d036f3667329d898a59f968316a9b16a6bd90
Murad501 <mm845635@gmail.com>
2022-10-26 01:43:44 +0600

    course category api created

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ const port = process.env.port || 5000
 
 app.use(cors());
 
+const category = require('./data/courseCategory.json')
+
 
 app.get('/', (req, res)=>{
     res.send('all course coming soon')
 })
 
+app.get('/category', (req, res) => {
+    res.send(category)
+})
 
 
 app.listen(port, ()=>{
